bd88bd403806ebbb14fa06e347402c283372f107
Antonio Loison <antonio.loison@student-cs.fr>
2019-06-11 23:01:14 +0100

    Create file for real-time sign detection

---
+++ Sign_recognition/video_process.py
@@ -0,0 +1,32 @@
+from neural_network import *
+from interface import *
+import cv2
+import numpy as np
+import time
+
+IMAGE_SIZE = 28*28*2
+
+cam = cv2.VideoCapture(0)
+NN_blue_circles = Nnetwork([IMAGE_SIZE, 8, 7, 3], [sigmoid, sigmoid, sigmoid, sigmoid])
+NN_rectangles = Nnetwork([IMAGE_SIZE, 6, 3], [sigmoid, sigmoid, sigmoid, sigmoid])
+NN_red_circles = Nnetwork([IMAGE_SIZE, 6, 4], [sigmoid, sigmoid, sigmoid, sigmoid])
+NN_red_triangles = Nnetwork([IMAGE_SIZE, 5, 5, 3], [sigmoid, sigmoid, sigmoid, sigmoid])
+NN_blue_circles.load(r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_weightsblue_circles.npy",
+                     r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_biasblue_circles.npy")
+NN_rectangles.load(r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_weightsrectangles.npy",
+                   r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_biasrectangles.npy")
+NN_red_circles.load(r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_weightsred_circles.npy",
+                   r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_biasred_circles.npy")
+NN_red_triangles.load(r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_weightsred_triangles.npy",
+                   r"C:\Users\Antonio\Documents\Projet_Autonomous_Driving\aadc\Sign_recognition\Neural_networks\saved_biasred_triangles.npy")
+
+neural_networks = {"trianglesred": NN_red_triangles, "rectanglesblue": NN_rectangles,
+                   "circlesred": NN_red_circles, "circlesblue": NN_blue_circles}
+
+while True:
+    ret, img=cam.read()
+    img=cv2.resize(img,(1060,int(220/340*1060)))
+
+    detected_signs(img, neural_networks)
+    cv2.imshow("cam",img)
+    cv2.waitKey(10)
